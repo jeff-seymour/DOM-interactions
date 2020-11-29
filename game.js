decb8411d3ad5374d431bb6652a03a3f93e1559c
Jeff <mojosodomo@gmail.com>
2020-11-29 23:05:25 +1300

    sprint3 t8

--- game.js
+++ game.js
@@ -12,6 +12,8 @@ function bindEventListeners (dots) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
     dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('click', makeBlue)
+    dots[i].addEventListener('dblclick', hide)
   }
 }
 
@@ -23,16 +25,28 @@ function makeGreen (evt) {
 
 // CREATE FUNCTION makeBlue HERE
 
+function makeBlue (evt) {
+  evt.target.classList.toggle('blue')
+  updateCounts()
+}
 // CREATE FUNCTION hide HERE
+function hide (evt) {
+  evt.target.classList.toggle('invisible')
+  updateCounts()
+}
 
 function updateCounts () {
   var totals = {
-    blue: 0,
+    blue: 0, 
     green: 0,
     invisible: 0
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
+  totals.blue = document.getElementsByClassName("blue").length;
+  totals.invisible = document.getElementsByClassName("invisible").length;
+  totals.green = document.getElementsByClassName("green").length;
+
 
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
@@ -41,5 +55,6 @@ function updateCounts () {
 function displayTotals (totals) {
   for (var key in totals) {
     document.getElementById(key + '-total').innerHTML = totals[key]
+
   }
 }
